23b430e708a56e39f019eeb5b61ba1ede49586e4
likezninjaz <estrueall@gmail.com>
2017-04-30 15:35:05 +0400

    Added Footer layout and scss styles for it

---
+++ app/js/components/footer.js
@@ -0,0 +1,62 @@
+var Footer = React.createClass({
+    render: function() {
+        return (
+            <section className="footer">
+                <div className="footer-links">
+                 <ul>
+                    <li>FEATURED SALE</li>
+                    <li>Alexis Hudson</li>
+                    <li>American Apparel</li>
+                    <li>Ben Sherman</li>
+                    <li>Big Buddha</li>
+                    <li>Chanel</li>
+                    <li>Christian Audigier</li>
+                    <li>Coach</li>
+                    <li>Cole Haan</li>
+                 </ul>
+                 <ul>
+                    <li>FEATURED SALE</li>
+                    <li>Alexis Hudson</li>
+                    <li>American Apparel</li>
+                    <li>Ben Sherman</li>
+                    <li>Big Buddha</li>
+                    <li>Chanel</li>
+                    <li>Christian Audigier</li>
+                    <li>Coach</li>
+                    <li>Cole Haan</li>
+                 </ul>
+                 <ul>
+                    <li>FEATURED SALE</li>
+                    <li>Alexis Hudson</li>
+                    <li>American Apparel</li>
+                    <li>Ben Sherman</li>
+                    <li>Big Buddha</li>
+                    <li>Chanel</li>
+                    <li>Christian Audigier</li>
+                    <li>Coach</li>
+                    <li>Cole Haan</li>
+                 </ul>
+                 <ul>
+                    <li>FEATURED SALE</li>
+                    <li>Alexis Hudson</li>
+                    <li>American Apparel</li>
+                    <li>Ben Sherman</li>
+                    <li>Big Buddha</li>
+                    <li>Chanel</li>
+                    <li>Christian Audigier</li>
+                    <li>Coach</li>
+                    <li>Cole Haan</li>
+                 </ul>
+                </div>
+                <div className="footer-copyright">
+                    Copyright 2017 Evgeny Studitskikh
+                </div>
+            </section>
+        );
+    }
+});
+
+ReactDOM.render(
+    <Footer />,
+    document.getElementById('footer')
+);
